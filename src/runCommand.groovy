e0a53ad471feff72930d443e32379b1f98ba1a7f
Praveen Talawar <praveen.talawar1992@gmail.com>
2018-11-19 13:44:40 +0000

    correction

--- src/runCommand.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/groovy
-package com.praveen;
-
-def runCommand(String cmd='echo run command'){
-	echo "write a command to run here"
-}
---
+++ vars/runCommand.groovy
@@ -0,0 +1,5 @@
+#!/usr/bin/groovy
+
+def runCommand(String cmd='echo run command'){
+	echo "write a command to run here"
+}
