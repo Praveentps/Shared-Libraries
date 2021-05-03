5a8a66dadc3be743dd68972717a05798c33d758e
ptalawar <praveen.talawar@gartner.com>
2021-05-03 18:52:49 +0530

    adding input step

--- src/buildUtils.groovy
+++ src/buildUtils.groovy
@@ -10,4 +10,15 @@ class buildUtils implements Serializable {
             steps.sh "mvn ${tasks}"
        }
     }
+
+    def inputStep(){
+    def userInput = input(
+            id: 'userInput', message: 'This is PRODUCTION!', parameters: [
+            [$class: 'BooleanParameterDefinition', defaultValue: false, description: '', name: 'Please confirm you sure to proceed']
+        ])
+
+        if(!userInput) {
+            error "Build wasn't confirmed"
+        }
+    }
 }
