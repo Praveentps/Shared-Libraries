aedc972d2d00b588261f6a63238002058a526876
Praveen Talawar <praveen.talawar1992@gmail.com>
2021-05-04 13:15:34 +0530

    adding debug

--- src/buildUtils.groovy
+++ src/buildUtils.groovy
@@ -3,6 +3,7 @@ class buildUtils implements Serializable {
     def steps
     buildUtils(steps) { this.steps = steps}
        def timedGradleBuild(tasks) {
+           print 'Hello world'
            steps.sh "echo hello from buildUtils"
        def gradleHome = steps.tool 'gradle3.2'
        steps.timestamps {
