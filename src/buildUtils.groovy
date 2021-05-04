56bc0568e542a61a1c23d512dd540612cf1e5d4b
Praveen Talawar <praveen.talawar1992@gmail.com>
2021-05-04 13:10:25 +0530

    adding debug

--- src/buildUtils.groovy
+++ src/buildUtils.groovy
@@ -1,24 +1,14 @@
 
-package praveen.com
-
 class buildUtils implements Serializable {
     def steps
     buildUtils(steps) { this.steps = steps}
        def timedGradleBuild(tasks) {
+           steps.sh "echo hello from buildUtils"
        def gradleHome = steps.tool 'gradle3.2'
        steps.timestamps {
             steps.sh "mvn ${tasks}"
        }
     }
 
-    def inputStep(){
-    def userInput = input(
-            id: 'userInput', message: 'This is PRODUCTION!', parameters: [
-            [$class: 'BooleanParameterDefinition', defaultValue: false, description: '', name: 'Please confirm you sure to proceed']
-        ])
-
-        if(!userInput) {
-            error "Build wasn't confirmed"
-        }
-    }
+  
 }
